59ad93d04833292ba6f317d633dfc736f29e5167
Adriano Marques <adriano.mqs@gmail.com>
2017-06-04 16:23:16 -0300

    Colocando Diferenciacao de Cor nas pecas do Tabuleiro

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,9 +11,9 @@ namespace ConsoleUI
             Tabuleiro tab = new Tabuleiro(8, 8);
             try { 
             tab.SetPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
-            tab.SetPeca(new Rei(tab, Cor.Preto), new Posicao(0, 1));
+            tab.SetPeca(new Rei(tab, Cor.Branco), new Posicao(0, 1));
             tab.SetPeca(new Torre(tab, Cor.Preto), new Posicao(1, 3));
-            tab.SetPeca(new Rei(tab, Cor.Preto), new Posicao(2, 4));
+            tab.SetPeca(new Rei(tab, Cor.Branco), new Posicao(2, 4));
 
             Tela.ImprimirTabuleiro(tab);
             }
--- ConsoleUI/Tela.cs
+++ ConsoleUI/Tela.cs
@@ -9,18 +9,38 @@ namespace ConsoleUI
         {
             for (int i=0; i<tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j=0; j<tab.Colunas; j++)
                 {
                     if (tab.Get(i, j) == null)
                     {
-                        Console.Write("_ ");
+                        Console.Write("- ");
                     }
                     else
                     {
-                        Console.Write(tab.Get(i, j) + " ");
+                        Tela.ImprimirPeca(tab.Get(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.Write("  a b c d e f g h");
+        }
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branco){
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
